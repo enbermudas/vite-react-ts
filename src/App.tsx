8d3f2547901ceb972865d1f198ebf5e4d53a15e1
Enrique Bermúdez <enbermudez96@gmail.com>
2023-09-30 19:18:35 -0300

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div id="app">App</div>;
+  return (
+    <div
+      id="app"
+      className="text-3xl font-bold bg-slate-900 w-screen h-screen p-4 text-white"
+    >
+      App
+    </div>
+  );
 }
 
 export default App;
